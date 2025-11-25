461d0824d073d8beb1d1f08d63d13005cba73223
RuthPina8 <124050978@upq.edu.mx>
2025-11-25 10:28:21 -0600

    Actualizacion
    Actualizacion de la pagina app.js en la cual se llevo a cabo la correccion de ciertas cosas

--- App.js
+++ App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-<<<<<<< HEAD
+
 import InicioScreen from './screens/InicioScreen';
 import HomeScreen from './screens/HomeScreen';
 import CategoriaScreen from './screens/CategoriaScreen';
-=======
-import InicioSesionScreen from './screens/InicioScreen';
-import Home from './screens/HomeScreen';
->>>>>>> 92b58516ec2db3e68ffa55f385cb5844d561f037
+import Registro from './screens/Registro'; 
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-<<<<<<< HEAD
-      <Stack.Navigator 
-        initialRouteName='InicioScreen'
+      <Stack.Navigator
+        initialRouteName="InicioScreen"
         screenOptions={{
           headerStyle: {
             backgroundColor: '#4CAF50',
@@ -29,36 +25,35 @@ export default function App() {
           },
         }}
       >
+
+       
         <Stack.Screen
           name="InicioScreen"
           component={InicioScreen}
           options={{ title: 'Ahorra+ App' }}
         />
+
+       
+        <Stack.Screen
+          name="Registro"
+          component={Registro}
+          options={{ title: 'Crear Cuenta' }}
+        />
+
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: 'Inicio' }}
         />
+
+    
         <Stack.Screen
           name="Categoria"
           component={CategoriaScreen}
           options={{ title: 'Gráficas por Categoría' }}
-=======
-      <Stack.Navigator initialRouteName='InicioScreen'>
-        <Stack.Screen
-          name="InicioScreen"
-          component={InicioSesionScreen}
         />
-        <Stack.Screen
-          name="Home"
-          component={Home}
->>>>>>> 92b58516ec2db3e68ffa55f385cb5844d561f037
-        />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 92b58516ec2db3e68ffa55f385cb5844d561f037
